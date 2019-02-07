0847090086104864a4663a66266cb0282c0c648c
borjasolerr <borjasolrey@gmail.com>
2019-02-07 15:19:30 +0100

    Design: Finished design for homepage, desktop and mobile

--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,28 @@ ScrollReveal().reveal(".header-image");
 ScrollReveal().reveal(".right-image_marketing");
 ScrollReveal().reveal(".feature-card");
 ScrollReveal().reveal(".left-image_marketing");
+
+// Login page
+
+$(document).ready(function() {
+  $(".tabs .tab").click(function() {
+    var tab_id = $(this).attr("data-tab");
+
+    $(".tabs .tab").removeClass("current");
+    $(".tab-content").removeClass("current");
+
+    $(this).addClass("current");
+    $("#" + tab_id).addClass("current");
+  });
+
+  $(".input").blur(function() {
+    tmpval = $(this).val();
+    if (tmpval == "") {
+      $(this).addClass("");
+      $(this).removeClass("not-empty");
+    } else {
+      $(this).addClass("not-empty");
+      $(this).removeClass("");
+    }
+  });
+});
